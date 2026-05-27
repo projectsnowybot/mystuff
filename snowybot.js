55dfb0e8784f51a70b9d9c7eda9b69bdb9032159
james william snow <178160119+projectsnowybot@users.noreply.github.com>
2026-05-27 19:26:21 +1000

    Add files via upload

--- snowybot.js
+++ snowybot.js
@@ -1,20 +1,20 @@
-const ALICE = Number(document.getElementById("pct_balance").value);
+const fanboy = Number(document.getElementById("pct_balance").value);
 document.getElementById("b_min").click();
-const bastard = (ALICE/144).toFixed(8);
-var beats = bastard; 
-const tenticle = (bastard*10);
-const fornication = (bastard*6.9);
-const greast = (bastard*7.9);
-var moron = (((Math.floor(ALICE/tenticle))*tenticle)-(tenticle*10));
-var baboons = ((Math.floor(ALICE/tenticle))*tenticle);
-var madman = parseFloat((Math.ceil(belance/tenticle))*tenticle);
-var OSCAR = ALICE;
-var belance = ALICE;
-var billy = ALICE;
-var scotty = ALICE; 
-var hugo = 6.9;
-var milly = 2.9;  
-var bogus =  parseFloat((beats*1).toFixed(8));
+const samuel = (fanboy/144000).toFixed(8);
+var amanku = samuel; 
+const ziggie = (samuel*10);
+const smile = (samuel*6.9);
+const buck = (samuel*7.9);
+var moron = fanboy;
+var baboons = ((Math.floor(fanboy/ziggie))*ziggie);
+var madman = ((Math.floor(fanboy/ziggie))*ziggie);
+var OSCAR = fanboy;
+var snowy = fanboy;
+var billy = fanboy;
+var scotty = fanboy; 
+var betfired = false 
+var fart = 1;
+var bogus =  parseFloat((amanku*1).toFixed(8));
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -22,26 +22,37 @@ function sleep(ms) {
 
 async function runFelineBot() {
     OSCAR = Number(document.getElementById("pct_balance").value);
-
     if ((OSCAR==billy) || (OSCAR==scotty)) {
-        belance = parseFloat(OSCAR); 
-        if (belance <= (bastard + (beats*4))){  
-            beats = bastard; 
-            baboons = ((Math.floor(belance/tenticle))*tenticle);
+        betfired = false;
+    } 
+    if (!betfired){
+        snowy = parseFloat(OSCAR); 
+
+        if (snowy >= (madman+(ziggie*fart))){ 
+            amanku = samuel; 
+            fart = 1;
+            baboons = ((Math.floor(snowy/ziggie))*ziggie);
+            madman = ((Math.floor(snowy/ziggie))*ziggie);
         } 
-        if ((belance > (((Math.floor(belance/tenticle))*tenticle)+fornication)) && (belance < (((Math.floor(belance/tenticle))*tenticle)+greast)) && (belance!==baboons)){
-            beats = beats*2;
-            baboons = parseFloat(belance); 
+        if ((snowy > (((Math.floor(snowy/ziggie))*ziggie)+smile)) && (snowy < (((Math.floor(snowy/ziggie))*ziggie)+buck)) && (snowy>baboons)){
+            amanku = amanku*2;
+            baboons = parseFloat(snowy); 
         }
-        bogus =  parseFloat((beats*1).toFixed(8));
-        billy = parseFloat(((belance + bogus)*1).toFixed(8));
-        scotty = parseFloat(((belance - bogus)*1).toFixed(8));
-        console.log("Profit:", ((belance - ALICE) * 1).toFixed(8));
+        if ((snowy > (((Math.floor(snowy/ziggie))*ziggie)+smile)) && (snowy < (((Math.floor(snowy/ziggie))*ziggie)+buck)) && (snowy<baboons)){
+            amanku = amanku*2;
+            fart = 0;
+            baboons = parseFloat(snowy); 
+        }
+        bogus =  parseFloat((amanku*1).toFixed(8));
+        console.log("Profit:", ((snowy - fanboy) * 1).toFixed(8));
         document.getElementById("pct_chance").value=49.5
-        document.getElementById("pct_bet").value = (beats*1).toFixed(8);
-        document.getElementById("a_lo").click(); 
+        document.getElementById("pct_bet").value = (amanku*1).toFixed(8);
+        billy = parseFloat(((snowy + bogus)*1).toFixed(8));
+        scotty = parseFloat(((snowy - bogus)*1).toFixed(8));
+        betfired = true;
+        document.getElementById("a_lo").click();
     }
 }
 
 
-setInterval(() => runFelineBot(), 1000)
+setInterval(() => runFelineBot(), 1)
