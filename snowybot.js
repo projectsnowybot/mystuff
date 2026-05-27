a4874677c094c31cbca3d02dc94de3be4e67bcd7
james william snow <178160119+projectsnowybot@users.noreply.github.com>
2026-05-27 10:13:30 +1000

    Add files via upload

--- snowybot.js
+++ snowybot.js
@@ -25,23 +25,20 @@ async function runFelineBot() {
 
     if ((OSCAR==billy) || (OSCAR==scotty)) {
         belance = parseFloat(OSCAR);
+        if (belance>madman){
+            madman = parseFloat(belance);
+        }
         if ((belance-(beats*2)) <= moron){  
             beats = bastard; 
             baboons = parseFloat((Math.floor(belance/tenticle))*tenticle);
             moron = parseFloat((Math.floor(belance/tenticle))*tenticle);
-            madman = parseFloat((Math.ceil(belance/tenticle))*tenticle);
+            madman = parseFloat(belance); 
         }
-        if ((belance>=madman) && ((belance-(beats*4)) <= moron)){  
+        if ((belance>=madman) && ((belance-((beats*4)+bastard)) <= moron)){  
             beats = bastard; 
             baboons = parseFloat((Math.floor(belance/tenticle))*tenticle);
-            madman = parseFloat((Math.ceil(belance/tenticle))*tenticle); 
-        }
-        if ((belance-moron) >= (tenticle*10)){
-            beats = bastard; 
-            baboons = parseFloat((Math.floor(belance/tenticle))*tenticle);
-            moron = parseFloat(((Math.floor(belance/tenticle))*tenticle)-tenticle); 
-            madman = parseFloat((Math.ceil(belance/tenticle))*tenticle);
-        }   
+            madman = parseFloat(belance); 
+        }  
         if ((belance > (((Math.floor(belance/tenticle))*tenticle)+fornication)) && (belance < (((Math.floor(belance/tenticle))*tenticle)+greast)) && (belance!==baboons)){
             beats = beats*2;
             baboons = parseFloat(belance); 
